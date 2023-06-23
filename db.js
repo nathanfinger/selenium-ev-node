227c480c765f4a7f014218fe1d665993339aeb69
Nathan <nathanfinger@gmail.com>
2023-06-23 16:46:52 -0300

    auto close browser + updated counter

--- db.js
+++ db.js
@@ -46,6 +46,29 @@ export async function setPropertyOnDoc(docData, propertyName, propertyValue) {
 }
 
 
+export async function countLivrosWithoutProperty(propertyName, propertyValue = false) {
+    const endpoint = "/local_mysql_expedicao/_find";
+    const requestBody = {
+        selector: {
+            $or: [
+                { [propertyName]: { $exists: false } },
+                { [propertyName]: { $eq: propertyValue } }
+            ]
+        },
+        fields: ["_id"],
+        limit: 0 // Set limit to 0 to retrieve only the count without fetching documents
+    };
+
+    try {
+        const response = await instance.post(endpoint, requestBody);
+        return response.data.docs.length;
+    } catch (error) {
+        console.error(error);
+        return 0; // Return 0 if an error occurs
+    }
+}
+
+
 
 export async function test(){
     let book = await getLivroWithoutProperty('removedFromEv',false)
