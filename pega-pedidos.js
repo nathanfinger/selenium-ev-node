32e0126d47462360ff46a683e96e143781c82328
Nathan <nathanfinger@gmail.com>
2023-07-21 13:19:20 -0300

    pega mais de uma página de pedidos

--- pega-pedidos.js
+++ pega-pedidos.js
@@ -1,4 +1,4 @@
-import {sleep, evPegaInfosPedido, evListaPedidos, evPgVendas, closeDriver,currentUrl} from './common.js'
+import {sleep, evPegaInfosPedido, evListaPedidos, evPgVendas, closeDriver, currentUrl, evListaStatus} from './common.js'
 import {updateOrSaveDoc} from './db.js'
 
 
@@ -6,10 +6,23 @@ import {updateOrSaveDoc} from './db.js'
 const startHandler = async ()=>{
 
     // página de pedidos
-    await evPgVendas()
+    await evPgVendas(1)
     let pedidos = await evListaPedidos()
-    console.log('pedidos identificados na página da EV:')
+    let pedidosStatus = await evListaStatus()
+
+    // pagina 2
+    await sleep(2)
+    await evPgVendas(2)
+    pedidos = pedidos.concat(await evListaPedidos())
+    pedidosStatus = pedidosStatus.concat(await evListaStatus())
+
+    console.log('Pedidos identificados:')
     console.log(pedidos)
+
+
+    // TODO filtrar status que não mudaram
+    // por aqui já é possível identificar o statusEv pra fazer o scraping só dos pedidos que mudaram statusEv
+    // console.log(pedidosStatus)
 
 
     // entrando na ediçaõ de cada um
@@ -18,7 +31,7 @@ const startHandler = async ()=>{
             await sleep(20)
         }
         try {
-            await evPgVendas() // simular navegação pra tentar evitar de derrubarem a sessão
+            // await evPgVendas() // simular navegação pra tentar evitar de derrubarem a sessão
             await sleep(1)
             let infos = await evPegaInfosPedido(nped)
             infos = {...infos, origem: 'ev'}
